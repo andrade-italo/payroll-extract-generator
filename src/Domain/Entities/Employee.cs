8befef1fe5f50a673fa094fe58ef6f1677fa654c
Italo Andrade <italoandrade13@gmail.com>
2024-02-28 21:28:05 -0300

    fix: change return of create employee

--- src/Domain/Entities/Employee.cs
+++ src/Domain/Entities/Employee.cs
@@ -2,15 +2,15 @@
 {
   public class Employee
   {
-    public long Id { get; private set; }
-    public string FirstName { get; private set; }
-    public string LastName { get; private set; }
-    public string Document { get; private set; }
-    public string Department { get; private set; }
-    public decimal GrossSalary { get; private set; }
-    public DateTime HireDate { get; private set; }
-    public bool HasHealthPlanDiscount { get; private set; }
-    public bool HasDentalPlanDiscount { get; private set; }
-    public bool HasTransportationVoucherDiscount { get; private set; }
+    public long Id { get; init; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Document { get; set; }
+    public string Department { get; set; }
+    public decimal GrossSalary { get; set; }
+    public DateTime HireDate { get; set; }
+    public bool HasHealthPlanDiscount { get; set; }
+    public bool HasDentalPlanDiscount { get; set; }
+    public bool HasTransportationVoucherDiscount { get; set; }
   }
 }
