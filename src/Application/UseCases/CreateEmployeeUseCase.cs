e0ddac5aadbaa445674962ae0b4ff99b613418ef
Italo Andrade <italoandrade13@gmail.com>
2024-02-28 21:29:24 -0300

    feat: create use cases

---
+++ src/Application/UseCases/CreateEmployeeUseCase.cs
@@ -0,0 +1,26 @@
+﻿using PayrollExtractGenerator.Application.Interfaces;
+using PayrollExtractGenerator.Application.DTOs;
+using PayrollExtractGenerator.Domain.Interfaces;
+using PayrollExtractGenerator.Application.Mappers;
+
+namespace PayrollExtractGenerator.Application.UseCases
+{
+  public class CreateEmployeeUseCase : ICreateEmployeeUseCase
+  {
+    private readonly IEmployeeRepository _employeeRepository;
+
+
+    public CreateEmployeeUseCase(IEmployeeRepository employeeRepository)
+    {
+      _employeeRepository = employeeRepository;
+    }
+
+    public async Task<EmployeeDTO> ExecuteAsync(EmployeeDTO input)
+    {
+      var employee = input.MapToEntity();
+      var employeeCreated = await _employeeRepository.AddAsync(employee);
+
+      return employeeCreated.MapToDto();
+    }
+  }
+}
