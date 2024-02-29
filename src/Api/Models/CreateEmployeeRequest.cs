f6dd3c3cd4b5cfa1ffdb77fb2908df74faa6485e
Italo Andrade <italoandrade13@gmail.com>
2024-02-28 21:39:42 -0300

    feat: create model

---
+++ src/Api/Models/CreateEmployeeRequest.cs
@@ -0,0 +1,15 @@
+namespace PayrollExtractGenerator.Api.Models
+{
+  public class CreateEmployeeRequest
+  {
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Document { get; set; }
+    public string Department { get; set; }
+    public decimal GrossSalary { get; set; }
+    public DateTime HireDate { get; set; }
+    public bool HasHealthPlanDiscount { get; set; }
+    public bool HasDentalPlanDiscount { get; set; }
+    public bool HasTransportationVoucherDiscount { get; set; }
+  }
+}
